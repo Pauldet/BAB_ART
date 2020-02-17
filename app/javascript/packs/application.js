c764cf6df646a49fa99f100f4115875e602e0f9b
Laurent Binder <laurentbinder@gmail.com>
2020-02-17 18:37:12 +0100

    css top-bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,51 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml5 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml5 .line',
+      opacity: [0.5,1],
+      scaleX: [0, 1],
+      easing: "easeInOutExpo",
+      duration: 700
+    }).add({
+      targets: '.ml5 .line',
+      duration: 600,
+      easing: "easeOutExpo",
+      translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+    }).add({
+      targets: '.ml5 .ampersand',
+      opacity: [0,1],
+      scaleY: [0.5, 1],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=600'
+    }).add({
+      targets: '.ml5 .letters-left',
+      opacity: [0,1],
+      translateX: ["0.5em", 0],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=300'
+    }).add({
+      targets: '.ml5 .letters-right',
+      opacity: [0,1],
+      translateX: ["-0.5em", 0],
+      easing: "easeOutExpo",
+      duration: 600,
+      offset: '-=600'
+    }).add({
+      targets: '.ml5',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
