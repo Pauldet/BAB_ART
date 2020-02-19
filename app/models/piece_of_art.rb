b76647096c021cd85a10b90ea399ef5982ae86b2
Paul de Thore <p_dethore@hotmail.com>
2020-02-19 10:20:29 +0100

    fixing carroussel display and other stuffs

--- app/models/piece_of_art.rb
+++ app/models/piece_of_art.rb
@@ -2,6 +2,7 @@ class PieceOfArt < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+  validates :name, :category, :daily_price, :artist_name, presence: true
 
   # def check_for_availability(date)
   #   TODO
