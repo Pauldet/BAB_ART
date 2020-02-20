3d8a85ef9660eef1947e0e0136a1484803518cac
Nicolas Martin-Bouyer <nicolas.martinbouyer@gmail.com>
2020-02-20 15:09:30 +0100

    profile page logic done

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,5 +4,11 @@ class ProfilesController < ApplicationController
   def show
   @user = current_user
   @piece_of_arts = @user.piece_of_arts
+  @bookings = @user.bookings
+    @pending_bookings = @user.bookings.where(status: 'pending')
+    @approved_bookings = @user.bookings.where(status: 'approved')
+    @ongoing_bookings = @user.bookings.where(status: 'ongoing' )
+    @past_bookings = @user.bookings.where(status: 'terminated')
+    @declined_bookings = @user.bookings.where(status: 'declined')
   end
 end
